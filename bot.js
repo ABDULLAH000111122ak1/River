43884c7b035a8f5b1d9f179d657e4b31cd3e809f
ABDULLAH000111122ak1 <43869515+ABDULLAH000111122ak1@users.noreply.github.com>
2018-10-19 07:10:21 +0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -12,42 +12,6 @@ client.user.setGame(`Death Shop`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
-client.on("message", async message => {
-        if(!message.channel.guild) return;
- var prefix= "+";
-        if(message.content.startsWith(prefix + 'server')) {
-        let guild = message.guild
-        let channel = message.channel
-        let guildicon = guild.icon_url
-        let members = guild.memberCount
-        let bots = guild.members.filter(m => m.user.bot).size
-        let humans = members - bots
-        let allchannels = guild.channels.size
-        let textchannels = guild.channels.filter(e => e.type === "text")
-        let voicechannels = guild.channels.filter(e => e.type === "voice")
-          var embed = new Discord.RichEmbed()
-          .setColor("#000000")
-          .setTitle(`معلومات عن السيرفر`)
-          .setDescription(`معلومات عن : ${guild.name}`)
-          .addField("صاحب السيرفر :", `${guild.owner}`, true)
-          .addField("أيدي السيرفر :", `${guild.id}`, true)
-          .addField("موقع السيرفر :", `${guild.region}`, true)
-          .addField("مستوى حماية السيرفر :", `${guild.verificationLevel}`, true)
-          .addField("عدد الرومات الصوتية :", `${voicechannels.size}`, true)
-          .addField("عدد الرومات الكتابية :", `${textchannels.size}`, true)
-          .addField("عدد اعضاء السيرفر :", `${members}`, true)
-          .addField("عدد البوتات :", `${bots}`, true)
-          .addField("عدد الاشخاص :", `${humans}`, true)
-          .addField("عدد رتب السيرفر :", `${guild.roles.size}`, true)
-          .addField(`أيموجيات الخاصة بالسيرفر : (${guild.emojis.size})`, `- ${guild.emojis.array()}`, true)
-          .setFooter(`تم انشاء هذه السيرفر في: ${guild.createdAt}`)
- 
-       message.channel.send({ embed: embed });
- 
-      }
-    });
-
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
 require("discord.js");
 const client = new Discord.Client();
 
@@ -75,3 +39,6 @@ const client = new Discord.Client();
       message.author.sendEmbed(Embed11)
     }
 }); 
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
